cfd93df901f36d053c3b905aa398890b7d065a16
Princess Pixel (Erika) <queercastle@gmail.com>
2024-01-24 00:59:41 -0800

    Gardens Implemented

--- Scripts/Rulesets/GardenRules.gd
+++ Scripts/Rulesets/GardenRules.gd
@@ -2,36 +2,27 @@ extends TileRuleset
 class_name GardenRules
 
 func UpdateHex(map:WorldMap, coords:Vector2i):
-	#var neighbors = map.GetAllAdjacent(coords)
-	##print("Starting Garden Spread")
-	#for tile in neighbors:
-		#if !map.hexDatabase.has(tile):
-			#continue
-		##prints(tile, map.hexDatabase[tile].tileType.name)
-		#if TagTrig(map, tile, "Fertile"):
-			##print("Found Fertile Land!")
-			#map.ChangeTile(tile, self)
-			#map.updateOrder.erase(tile)
-	
 	var neighbors = map.GetAllAdjacent(coords)
-	#print(coords, "'s Forest Counter is at: ", map.hexDatabase[coords].counter)
-	var RNG:int
-	var RNGtile:Vector2i
-	#For each stack of garden tile, propogate once. If propogates into garden, increase stack of target.
-	for i in range(map.hexDatabase[coords].stackCount):
-		RNG = RandomNumberGenerator.new().randi_range(0, neighbors.size()-1)
-		RNGtile = neighbors[RNG]
-		if map.hexDatabase.has(RNGtile):
-			#prints(tile, map.hexDatabase[tile].tileType.name)
-			if TagTrig(map, RNGtile, "Fertile"): #If fertile tile found, change tile to garden stack 1
-				#print("Found Fertile Land! Growing Garden.")
-				map.ChangeTile(RNGtile, HexTypes.type["Garden"])
-				map.updateOrder.erase(RNGtile)
-			elif TileTrig(map, RNGtile, HexTypes.type["Garden"]):
-				print("Found Garden Tile, growing stacks on target.")
-				map.ChangeStack(RNGtile, 1)
+	var smallestGarden:int = 0
+	var gardenCount:int = 0
+	for tile in neighbors:
+		if !map.hexDatabase.has(tile):
+			continue
+		if TileTrig(map, tile, HexTypes.type["Garden"]):
+			gardenCount += 1
+			if smallestGarden == 0:
+				smallestGarden = map.hexDatabase[tile].stackCount
+			else:
+				smallestGarden = mini(smallestGarden, map.hexDatabase[tile].stackCount)
+		if TagTrig(map, tile, "Fertile") and TagTrig(map, tile, "Open"): #Brush spreads to fertile
+			if !CompareTrig(map, coords, tile, true): #Only if stack is lower
+				map.ChangeTile(tile, HexTypes.type["Garden"])
+				map.updateOrder.erase(tile)
+				continue
+	if gardenCount == 6:
+		#print("Smallest Garden is ", smallestGarden)
+		if map.hexDatabase[coords].stackCount <= smallestGarden:
+			map.ChangeStack(coords, 1)
+		
 	
-	
-	pass #For subresource, list all possible trigger/effect combos as ordered if statements
-	#if triggerX(map, coords) == true:
-	#	doEventY(tiletype)
+	#Increase Honey Resource by Stack Count
